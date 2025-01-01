b3ca1b4f140cb1ceb5c726091d562c78183f1528
tranthang98 <tranthang98>
2025-01-02 00:00:27 +0700

    work with JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,12 @@
+// JSX
 const MyComponent = () => {
-  return (
-    <div>thang & hoidanit</div>
-  );
+    return (
+        <>
+            <div>thang & hoidanit</div>
+            <div style={{color: "red"}}
+            >child</div>
+        </>
+    );
 }
 
 export default MyComponent;
