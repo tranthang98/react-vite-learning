9893abcbdbf1145b6a6f03fbd8ec1601612d76f8
tranthang98 <tranthang98>
2025-01-02 23:31:27 +0700

    JSX variables

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,26 @@
 // JSX
 const MyComponent = () => {
+    // const username = "thang";
+    // const username = 25;
+    // const username = true;
+    // const username = null;
+    // const username = undefined;
+    const username = [1, 2, 3];
+    // const username = {
+    //     name: "thangtn8",
+    //     age: 27
+    // }
+
     return (
         <>
-            <div>thang & hoidanit</div>
-            <div style={{color: "red"}}
+            <div>{JSON.stringify(username)} & hoidanit</div>
+            <div>{console.log("ERIC")}</div>
+            <div style={
+                {
+                    color: "red",
+                    borderRadius: "10px"
+                }
+            }
             >child</div>
         </>
     );
