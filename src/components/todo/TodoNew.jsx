b1b7e1d33c03e73394e316bf6f0efbf3e89905fa
tranthang98 <tranthang98>
2025-01-07 23:35:33 +0700

    props: pass function from class to subclass

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,4 +1,8 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> check point: ", props)
+    const {addNewTodo} = props
+
+    // addNewTodo("Alan");
     return (
         <div className='todo-new'>
             <input type="text" />
