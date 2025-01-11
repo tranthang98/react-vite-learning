95430d6e09b71249d0c421026ffb9124767fcc91
Thang Tran <tranthang98hust@gmail.com>
2025-01-11 14:31:09 +0700

    each child in a list should have a unique key prop

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,9 @@ import { useState } from 'react'
 const App = () => {
 
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Youtube" }
+    // { id: 1, name: "Learning React" },
+    // { id: 2, name: "Watching Youtube" }
   ])
-
-  const hoidanit = "Alan";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam"
-  }
 
   const addNewTodo = (name) => {
     const newTodo = {
@@ -37,9 +30,6 @@ const App = () => {
         addNewTodo={addNewTodo}
       />
       <TodoData
-        name={hoidanit}
-        age={age}
-        data={data}
         todoList={todoList}
       />
       <div className='todo-image'>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -6,21 +6,16 @@ const TodoData = (props) => {
     // const name = props.name;
     // const age = props.age;
     // const data = props.data;
-    console.log(">>> check props: ", props)
     return (
         <div className='todo-data'>
             {todoList.map((item, index) => {
-                console.log(">>> check map: ", item, index);
                 return (
-                    <div className="todo-item">
+                    <div className={`todo-item`} key={item.id}>
                         <div>{item.name}</div>
                         <button>Delete</button>
                     </div>
                 )
             })}
-            <div>
-                {JSON.stringify(todoList)}
-            </div>
         </div>
     )
 }
