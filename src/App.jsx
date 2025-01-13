4e2d8fdc38ea0ab424fff8558a513c8209ad9849
Thang Tran <tranthang98hust@gmail.com>
2025-01-13 13:48:29 +0700

    implement Index Route in ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
 import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData'
-import TodoNew from './components/todo/TodoNew'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
 import { Outlet } from 'react-router-dom'
 
 const App = () => {
-
-  const [todoList, setTodoList] = useState([
-    // { id: 1, name: "Learning React" },
-    // { id: 2, name: "Watching Youtube" }
-  ])
-
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id: randomIntFromInterval(1, 1000000),
-      name: name
-    }
-    setTodoList([...todoList, newTodo])
-  }
-
-  const deleteTodo = (id) => {
-    const newTodo = todoList.filter(item => item.id !== id)
-    setTodoList(newTodo)
-  }
-
-  const randomIntFromInterval = (min, max) => { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
   return (
     <>
       <Header />
-      <div className='todo-container'>
-        <div className='todo-title'>Todo List</div>
-        <TodoNew
-          addNewTodo={addNewTodo}
-        />
-        {todoList.length > 0 ?
-          <TodoData
-            todoList={todoList}
-            deleteTodo={deleteTodo}
-          />
-          :
-          <div className='todo-image'>
-            <img src={reactLogo} className='logo' />
-          </div>
-        }
-      </div>
       <Outlet />
       <Footer />
     </>
