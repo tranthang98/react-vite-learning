714eb78ebd65d1a169bdba607c24e1de38b72291
Thang Tran <tranthang98hust@gmail.com>
2025-01-13 17:11:17 +0700

    makeup error.jsx page, add ant-design/icons + react-jss

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,11 +1,12 @@
 import { Link, useRouteError } from "react-router-dom";
+import { createUseStyles } from 'react-jss'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
-  return (
-    <div id="error-page" style={{
+  const useStyles = createUseStyles({
+    errorPage: {
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
@@ -16,34 +17,49 @@ export default function ErrorPage() {
       textAlign: "center",
       fontFamily: "Arial, sans-serif",
       padding: "20px",
-    }}>
-      <h1 style={{
-        fontSize: "4rem",
-        fontWeight: "bold",
-        marginBottom: "10px",
-        color: "#ff6b6b",
-      }}>Oops!</h1>
-      <p style={{
-        fontSize: "1.2rem",
-        marginBottom: "20px",
-      }}>Sorry, an unexpected error has occurred.</p>
-      {error && <p style={{
-        fontStyle: "italic",
-        color: "#555",
-        marginBottom: "20px",
-      }}>{error.statusText || error.message}</p>}
+    },
+    errorH1: {
+      fontSize: "4rem",
+      fontWeight: "bold",
+      marginBottom: "10px",
+      color: "#ff6b6b"
+    },
+    errorP: {
+      fontSize: "1.2rem",
+      marginBottom: "20px"
+    },
+    error2ndP: {
+      fontStyle: "italic",
+      color: "#555",
+      marginBottom: "20px"
+    },
+    errorLink: {
+      display: "inline-block",
+      padding: "10px 20px",
+      fontSize: "1rem",
+      color: "#fff",
+      backgroundColor: "#007bff",
+      borderRadius: "5px",
+      textDecoration: "none",
+      transition: "background-color 0.3s ease"
+    }
+  })
+
+  const classes = useStyles();
+  return (
+    <div className={classes.errorPage}>
+      <h1 className={classes.errorH1}>Oops!</h1>
+      <p className={classes.errorP}>Sorry, an unexpected error has occurred.</p>
+      {
+        error
+        &&
+        <p className={classes.error2ndP}>
+          {error.statusText || error.message}
+        </p>
+      }
       <Link
         to="/"
-        style={{
-          display: "inline-block",
-          padding: "10px 20px",
-          fontSize: "1rem",
-          color: "#fff",
-          backgroundColor: "#007bff",
-          borderRadius: "5px",
-          textDecoration: "none",
-          transition: "background-color 0.3s ease",
-        }}
+        className={classes.errorLink}
         onMouseEnter={(e) => (e.target.style.backgroundColor = "#0056b3")}
         onMouseLeave={(e) => (e.target.style.backgroundColor = "#007bff")}
       >
