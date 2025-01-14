ae20d26e15931a1895972275db91522bae6d0116
Thang Tran <tranthang98hust@gmail.com>
2025-01-14 17:47:25 +0700

    useEffect Hook

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,80 +1,47 @@
-import { Space, Table, Tag } from 'antd';
+import { Table } from 'antd';
+import { fetchAllUserAPI } from '../../services/api.service';
+import { useEffect, useState } from 'react';
 
 const UserTable = () => {
+
+    const [dataUsers, setDataUsers] = useState([
+        { _id: "thangtn8", fullName: "26", email: "hn" },
+        { _id: "tranthang98", fullName: "26", email: "hcm" }
+    ])
+
+    useEffect(() => {
+        console.log(">>> run useEffect 111");
+        loadUser();
+    }, []);
+
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            render: (text) => <a>{text}</a>,
+            title: 'Id',
+            dataIndex: '_id',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Full Name',
+            dataIndex: 'fullName',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map((tag) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <a>Invite {record.name}</a>
-                    <a>Delete</a>
-                </Space>
-            ),
-        },
-    ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
+            title: 'Email',
+            dataIndex: 'email',
+        }
     ];
 
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI();
+        setDataUsers(res.data)
+    }
+
+    console.log(">>> run render 000");
+
     return (
-        <Table columns={columns} dataSource={data} />
+        <Table
+            columns={columns}
+            dataSource={dataUsers}
+            rowKey={"_id"}
+        />
     )
 }
 
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -15,6 +15,11 @@ const updateUserAPI = () => {
 
 }
 
+const fetchAllUserAPI = () => {
+  const URL_BACKEND = "/api/v1/user";
+  return axios.get(URL_BACKEND);
+}
+
 export {
-  createUserAPI, updateUserAPI
+  createUserAPI, updateUserAPI, fetchAllUserAPI
 }
