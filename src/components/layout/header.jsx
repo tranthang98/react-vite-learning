4562874a0f31c86a08cc844b465a2837c0880146
Thang Tran <tranthang98hust@gmail.com>
2025-01-15 13:20:22 +0700

    useEffect with dependency, implement update user

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,15 +1,41 @@
-import { NavLink } from 'react-router-dom';
-import './header.css'
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import { AuditOutlined, HomeOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: 'users',
+            icon: <UsergroupAddOutlined />,
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: 'books',
+            icon: <AuditOutlined />
+        }
+    ];
+
+    const [current, setCurrent] = useState('');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
     return (
-        <div className='hoidanit'>
-            <ul>
-                <li><NavLink class="active" to="/">Home</NavLink></li>
-                <li><NavLink to="/users">Users</NavLink></li>
-                <li><NavLink to="/books">Books</NavLink></li>
-            </ul>
-        </div>
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+        />
     )
 }
 
