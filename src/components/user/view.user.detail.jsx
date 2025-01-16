f4a5f7124c94de3bc54991d07fba856b0a9dd895
Thang Tran <tranthang98hust@gmail.com>
2025-01-16 11:22:40 +0700

    display user avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,38 +1,94 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  drawerContent: {
+    fontFamily: "Arial, sans-serif",
+    color: "#333",
+    lineHeight: 1.6,
+    padding: "10px",
+  },
+  title: {
+    fontSize: "22px",
+    fontWeight: "bold",
+    color: "#0073e6",
+    textAlign: "center",
+    marginBottom: "20px",
+  },
+  paragraph: {
+    margin: "10px 0",
+    fontSize: "16px",
+  },
+  divider: {
+    borderBottom: "1px solid #ddd",
+    margin: "15px 0",
+  },
+  avatarContainer: {
+    textAlign: "center",
+  },
+  avatarImage: {
+    borderRadius: "8px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    display: "block",
+    margin: "0 auto",
+    width: "150x",
+    height: "100px",
+  },
+});
 
 const ViewUserDetail = (props) => {
-
-  const {
-    dataDetail,
-    setDataDetail,
-    isDetailOpen,
-    setIsDetailOpen
-  } = props;
+  const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+  const classes = useStyles();
 
   return (
     <Drawer
-      title="Chi tiết User"
+      width={"30vw"}
+      title={<div className={classes.title}>Chi tiết User</div>}
       onClose={() => {
         setDataDetail(null);
         setIsDetailOpen(false);
       }}
       open={isDetailOpen}
     >
-      {dataDetail ? <>
-        <p>Id: {dataDetail._id}</p>
-        <br />
-        <p>Full name: {dataDetail.fullName}</p>
-        <br />
-        <p>Email: {dataDetail.email}</p>
-        <br />
-        <p>Phone number: {dataDetail.phone}</p>
-      </>
-        :
-        <>
-          <p>Không có dữ liệu</p>
-        </>}
+      {dataDetail ? (
+        <div className={classes.drawerContent}>
+          <p className={classes.paragraph}>Id: {dataDetail._id}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Full name: {dataDetail.fullName}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Email: {dataDetail.email}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Phone number: {dataDetail.phone}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Avatar:</p>
+          <div className={classes.avatarContainer}>
+            <img
+              className={classes.avatarImage}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+              alt="User Avatar"
+            />
+          </div>
+          <div>
+            <label htmlFor="btnUpload" style={{
+              display: "block",
+              width: "fit-content",
+              marginTop: "15px",
+              padding: "5px 10px",
+              background: "orange",
+              borderRadius: "5px",
+              cursor: "pointer"
+            }}>
+              Upload Avatar
+            </label>
+            <input type="file" hidden id="btnUpload" />
+          </div>
+          {/* <Button type="primary">Upload Avatar</Button> */}
+        </div>
+      ) : (
+        <p>Không có dữ liệu</p>
+      )}
     </Drawer>
-  )
-}
+  );
+};
 
 export default ViewUserDetail;
--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,70 +1,79 @@
-import { Link, useRouteError } from "react-router-dom";
-import { createUseStyles } from 'react-jss'
+import { Drawer } from "antd";
+import { createUseStyles } from "react-jss";
 
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+const useStyles = createUseStyles({
+  drawerContent: {
+    fontFamily: "Arial, sans-serif",
+    color: "#333",
+    lineHeight: 1.6,
+    padding: "10px",
+  },
+  title: {
+    fontSize: "22px",
+    fontWeight: "bold",
+    color: "#0073e6",
+    textAlign: "center",
+    marginBottom: "20px",
+  },
+  paragraph: {
+    margin: "10px 0",
+    fontSize: "16px",
+  },
+  divider: {
+    borderBottom: "1px solid #ddd",
+    margin: "15px 0",
+  },
+  avatarContainer: {
+    textAlign: "center",
+  },
+  avatarImage: {
+    borderRadius: "8px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    display: "block",
+    margin: "0 auto",
+    width: "300px",
+    height: "250px",
+  },
+});
 
-  const useStyles = createUseStyles({
-    errorPage: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      backgroundColor: "#f8f9fa",
-      color: "#333",
-      textAlign: "center",
-      fontFamily: "Arial, sans-serif",
-      padding: "20px",
-    },
-    errorH1: {
-      fontSize: "4rem",
-      fontWeight: "bold",
-      marginBottom: "10px",
-      color: "#ff6b6b"
-    },
-    errorP: {
-      fontSize: "1.2rem",
-      marginBottom: "20px"
-    },
-    error2ndP: {
-      fontStyle: "italic",
-      color: "#555",
-      marginBottom: "20px"
-    },
-    errorLink: {
-      display: "inline-block",
-      padding: "10px 20px",
-      fontSize: "1rem",
-      color: "#fff",
-      backgroundColor: "#007bff",
-      borderRadius: "5px",
-      textDecoration: "none",
-      transition: "background-color 0.3s ease"
-    }
-  })
-
+const ViewUserDetail = (props) => {
+  const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
   const classes = useStyles();
+
   return (
-    <div className={classes.errorPage}>
-      <h1 className={classes.errorH1}>Oops!</h1>
-      <p className={classes.errorP}>Sorry, an unexpected error has occurred.</p>
-      {
-        error
-        &&
-        <p className={classes.error2ndP}>
-          {error.statusText || error.message}
-        </p>
-      }
-      <Link
-        to="/"
-        className={classes.errorLink}
-        onMouseEnter={(e) => (e.target.style.backgroundColor = "#0056b3")}
-        onMouseLeave={(e) => (e.target.style.backgroundColor = "#007bff")}
-      >
-        Back to homepage
-      </Link>
-    </div>
+    <Drawer
+      width={"30vw"}
+      title={<div className={classes.title}>Chi tiết User</div>}
+      onClose={() => {
+        setDataDetail(null);
+        setIsDetailOpen(false);
+      }}
+      open={isDetailOpen}
+    >
+      {dataDetail ? (
+        <div className={classes.drawerContent}>
+          <p className={classes.paragraph}>Id: {dataDetail._id}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Full name: {dataDetail.fullName}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Email: {dataDetail.email}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Phone number: {dataDetail.phone}</p>
+          <div className={classes.divider}></div>
+          <p className={classes.paragraph}>Avatar:</p>
+          <div className={classes.avatarContainer}>
+            <img
+              className={classes.avatarImage}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+              alt="User Avatar"
+            />
+          </div>
+        </div>
+      ) : (
+        <p>Không có dữ liệu</p>
+      )}
+    </Drawer>
   );
-}
+};
+
+export default ViewUserDetail;
