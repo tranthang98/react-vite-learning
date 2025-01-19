52cf48c410912fd3c3cf99ee1852a96f270b9718
tranthang98 <tranthang98>
2025-01-19 23:51:50 +0700

    uncontrolled component for register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,92 @@
+import { Button, Form, Input } from "antd";
 
 const RegisterPage = () => {
+
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(values);
+    };
+
     return (
-        <div>Register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            name="basic"
+            // labelCol={{
+            //     span: 8,
+            // }}
+            // wrapperCol={{
+            //     span: 16,
+            // }}
+            // style={{
+            //     maxWidth: 600,
+            // }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <div style={{
+                margin: "50px",
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phoneNumber"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary">Register</Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
