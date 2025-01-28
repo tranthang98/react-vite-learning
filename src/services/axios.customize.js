3ae14888e9dda3551e8e89642306341c7fd6d9a7
Thang Tran <tranthang98hust@gmail.com>
2025-01-28 23:07:37 +0700

    add loading bar

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+import NProgress from 'nprogress';
+
+NProgress.configure({
+  showSpinner: false,
+  trickleSpeed: 100,
+});
+
 // Set config defaults when creating the instance
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL
@@ -10,6 +17,7 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+  NProgress.start();
   if (typeof window !== "undefined" && window
     && window.localStorage
     && window.localStorage.getItem('access_token')) {
@@ -18,17 +26,20 @@ instance.interceptors.request.use(function (config) {
   // Do something before request is sent
   return config;
 }, function (error) {
+  NProgress.done();
   // Do something with request error
   return Promise.reject(error);
 });
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+  NProgress.done();
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
   if (response.data && response.data.data) return response.data;
   return response;
 }, function (error) {
+  NProgress.done();
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
   if (error.response && error.response.data) return error.response.data;
